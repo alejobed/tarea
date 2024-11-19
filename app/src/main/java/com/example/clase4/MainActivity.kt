2a223351c42573c0d7fc98a48e1db7a3a9ed843c
alejobed <alejobedoyam2008@gmail.com>
2024-11-19 15:49:23 -0500

    primer commit

---
+++ app/src/main/java/com/example/clase4/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.clase4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        //setContentView(R.layout.frame_layout)
+        setContentView(R.layout.constraint_layout)
+    }
+}
